699006d42235cd89ec67403f89bafc80bf213c1e
Nick Meyvis <nick.meyvis@gmail.com>
2017-03-20 02:13:52 -0400

    add toString

--- src/main/java/com/rfgbot/anywhereparse/Command.java
+++ src/main/java/com/rfgbot/anywhereparse/Command.java
@@ -3,12 +3,28 @@ package com.rfgbot.anywhereparse;
 import java.util.List;
 
 /**
+ *
  * Created by nickm on 3/11/2017.
  */
 public class Command {
+    /**
+     * The addon alias that triggered this command
+     */
     private String alias;
+
+    /**
+     * The list of parameters
+     */
     private List<String> parameters;
+
+    /**
+     * Index in the source array where this command originated
+     */
     private int start;
+
+    /**
+     * Index in the source array where this command terminated
+     */
     private int end;
 
     public Command(String alias, List<String> parameters, int start, int end) {
@@ -36,5 +52,10 @@ public class Command {
 
     public int getEnd() {
         return end;
+    }
+
+    @Override
+    public String toString() {
+        return alias + "(" + String.join(", ", parameters) + ")@[" + start + ", " + end + "]";
     }
 }
